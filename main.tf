3e23ed62c757a8be29914964270f26c37f18ae71
Naveen-manokaran <naveen.manokaran@bootlabstech.com>
2022-09-19 19:57:17 +0530

    recheck the module

--- main.tf
+++ main.tf
@@ -15,9 +15,9 @@ resource "azurerm_storage_account" "sa" {
   resource_group_name = var.resource_group_name
   location            = var.location
 
-  account_tier              = "Standard"
-  account_kind              = "StorageV2"
-  account_replication_type  = "LRS"
+  account_tier              = var.account_tier
+  account_kind              = var.account_kind
+  account_replication_type  = var.account_replication_type
   enable_https_traffic_only = true
 }
 
--- variables.tf
+++ variables.tf
@@ -32,3 +32,27 @@ variable "flow_log_name" {
   type        = string
   description = "name of the azurerm_network_watcher_flow_log"
 }
+
+variable "account_tier" {
+  type = string
+  description = "Defines the Tier to use for this storage account. For BlockBlobStorage and FileStorage accounts only Premium is valid."
+  default = "Standard"
+}
+
+variable "account_kind" {
+  type = string
+  description = "Defines the Kind of account. Valid options are BlobStorage, BlockBlobStorage, FileStorage, Storage and StorageV2"
+  default = "StorageV2"
+}
+
+variable "account_replication_type" {
+  type = string
+  description = "Defines the type of replication to use for this storage account. Valid options are LRS, GRS, RAGRS, ZRS, GZRS and RAGZRS."
+  default = "LRS"
+}
+
+variable "sku" {
+  type = string
+  description = "(optional) describe your variable"
+  default = "PerGB2018"
+}
